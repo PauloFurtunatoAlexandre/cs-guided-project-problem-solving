d305508f6bcd611971a526614129c889d26820dd
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-10-30 18:55:16 -0500

    solved demo 8

--- src/demonstration_08.py
+++ src/demonstration_08.py
@@ -8,8 +8,17 @@ Examples:
 - parity(0) -> "Even"
 - parity(1) -> "Odd"
 - parity(2) -> "Even"
+
+# check if argument is odd and return "Odd"
+# check if argument is even and return "Even"
+
 """
 def parity(input_int):
-    # Your code here
+    if input_int % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
 
-
+print(parity(0)) # -> "Even"
+print(parity(1)) # -> "Odd"
+print(parity(2)) # -> "Even"
