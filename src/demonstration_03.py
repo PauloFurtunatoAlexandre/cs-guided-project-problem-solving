a3b4a98ad7649277ac1aada216b499bf3a3adb9a
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-10-30 15:23:43 -0500

    planned and solved the demo 3

--- src/demonstration_03.py
+++ src/demonstration_03.py
@@ -11,15 +11,27 @@ Examples:
 
 Notes:
 - Bonus: Try to complete this challenge in one line!
+# transfor the string into numbers separated by commas
+    # need a variable to hold the new value, possible usage of map to create a new list of numbers with split those numbers
+# calculate the product of those numbers
+    
 """
 
 
 def multiply_nums(nums):
-    # Your code here
-    pass
+    new_values = []
+    product = 1
+    
+    new_values = list(map(int, nums.split(", ")))
+    
+    for i in new_values:
+        product = product * i
+    
+    return product
+    
 
 
-print(multiply_nums("2, 3")) # ➞ 6
-print(multiply_nums("1, 2, 3, 4")) # ➞ 24
-print(multiply_nums("54, 75, 453, 0")) # ➞ 0
-print(multiply_nums("10, -2")) # ➞ -20
+print(multiply_nums("2, 3"))  # ➞ 6
+print(multiply_nums("1, 2, 3, 4"))  # ➞ 24
+print(multiply_nums("54, 75, 453, 0"))  # ➞ 0
+print(multiply_nums("10, -2"))  # ➞ -20
