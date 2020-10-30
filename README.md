59e32c57767701a7ff79c176e1c265c7618e0649
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-10-29 21:26:50 -0500

    added understand, planning and solution of demo 01

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 This is the starter code for Computer Science - Sprint 1: Computer Science Fundamentals - Module 2: Problem Solving.
 
 Please fork and clone this repo to your computer by the start of class.
+
+(S).U.P.E.R.
+- (Stop)
+- Understand
+- Plan
+- Execute
+- Reflect
--- src/demonstration_01.py
+++ src/demonstration_01.py
@@ -12,9 +12,51 @@ Examples:
 Notes:
 - Return "invalid" if n exceeds the length of the list.
 - Return an empty list if n == 0.
+
+(reverse)
+Write a function that retrieves the first n elements from a list
+- Return "invalid" if n exceeds the length of the list.
+- Return an empty list if n == 0.
+
+- [0 to n] python we have a slice (takes a sub section of a list and returns that data as a list) [start inclusive : end exclusive]
+
+# check if is greater than the length of the list
+    # then return "invalid"
+# otherwise if n is equal to zero
+    # return an empty list
+otherwise
+    # return the slice of [0 : n]
+    
+
+(forward)
+Write a function that retrieves the first n elements from a list
+- Return "invalid" if n exceeds the length of the list.
+- Return an empty list if n == 0.
+
+- [0 to n] python we have a slice (takes a sub section of a list and returns that data as a list) [start inclusive : end exclusive]
+
+# check if is greater than the length of the list
+    # then return "invalid"
+# otherwise if n is equal to zero
+    # return an empty list
+otherwise
+    # return the slice of [length of the list - (n - 1): length of list + 1]
+    # return the slice of [0 : n]
 """
 
 
 def last(a, n):
     # Your code here
+    if n > len(a):
+        return "invalid"
+    elif n == 0:
+        return []
+    else:
+        return a[len(a) - n: len(a) + 1]
+        # return a[-n:]
 
+
+print(last([1, 2, 3, 4, 5], 1))  # ➞ [5]
+print(last([4, 3, 9, 9, 7, 6], 3))  # ➞ [9, 7, 6]
+print(last([1, 2, 3, 4, 5], 7))  # ➞ "invalid"
+print(last([1, 2, 3, 4, 5], 0))  # ➞ []
