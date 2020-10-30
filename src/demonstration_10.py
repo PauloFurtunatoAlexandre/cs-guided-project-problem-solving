bed5dceb7909351945935b444490e421740489f0
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-10-30 18:40:40 -0500

    solved demo 10

--- src/demonstration_10.py
+++ src/demonstration_10.py
@@ -14,8 +14,23 @@ Notes:
 - There will always be at least one number in the input string.
 - The return string must be two numbers separated by a single space, and
 the maximum number is first.
+
+# transform the argument in a list of integers in a new variable called numbers
+# sort the numbers and reverse them to find the max and min
+# test max and min in this list and return them using a join
+# concatenate max and min into a string
+
 """
+
+
 def max_and_min(input_str):
-    # Your code here
+    numbers = list(map(int, input_str.split(" ")))
+    numbers = sorted(numbers, reverse=True)
+    max_and_min_numbers = [numbers[0], numbers[-1]]
+
+    return " ".join([str(el) for el in max_and_min_numbers])
 
 
+print(max_and_min("1 2 3 4 5"))  # -> "5 1"
+print(max_and_min("1 2 -3 4 5"))  # -> "5 -3"
+print(max_and_min("1 9 3 4 -5"))  # -> "9 -5"
