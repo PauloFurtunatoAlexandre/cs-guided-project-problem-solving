03f61a7bd24636bdeacaf3b6b1bdf74b2bc1ec2a
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-11-01 13:06:02 -0600

    solved the demo 7

--- src/demonstration_07.py
+++ src/demonstration_07.py
@@ -11,7 +11,25 @@ Examples:
 - repeat_it("abcd") -> "A-Bb-Ccc-Dddd"
 - repeat_it("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 - repeat_it("cwAt") -> "C-Ww-Aaa-Tttt"
-"""
-def repeat_it(input_str):
-    # Your code here
 
+# create a new list where you can enumerate the characters by bringing each character multiplied by value of its current index
+# capitalize the first letter on each segment
+# join all segments in a string again using the "-" character
+
+
+"""
+
+
+def repeat_it(input_str):
+    letters_list = []
+    for k, v in enumerate(input_str):
+        letters_list.append((((k+1) * v)).capitalize())
+
+    input_str = "-".join(letters_list)
+
+    return input_str
+
+
+print(repeat_it("abcd"))  # -> "A-Bb-Ccc-Dddd"
+print(repeat_it("RqaEzty"))  # -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+print(repeat_it("cwAt"))  # -> "C-Ww-Aaa-Tttt"
